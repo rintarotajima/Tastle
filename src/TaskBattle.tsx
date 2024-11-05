59cbc63d26c3086209c415c0fedf1e70a50644e0
rintarotajima <rintarotajima@gmail.com>
2024-11-05 23:37:14 +0900

    [ add ] Set props for each component of the TaskBattle.tsx

--- src/TaskBattle.tsx
+++ src/TaskBattle.tsx
@@ -1,3 +1,5 @@
+import { FC } from "react";
+
 const Title = () => {
   return (
     <>
@@ -10,7 +12,11 @@ const Title = () => {
   );
 };
 
-const InputTask = () => {
+type InputTaskProps = {
+  onAddTask: () => void;
+};
+
+const InputTask: FC<InputTaskProps> = ({ onAddTask }) => {
   return (
     <>
       <section className="text-center mb-10 space-x-4 max-w-2xl w-full mx-auto">
@@ -19,7 +25,10 @@ const InputTask = () => {
           type="text"
           placeholder="タスクを追加"
         />
-        <button className="text-lg text-white bg-orange-300 hover:bg-orange-500 rounded-md p-2.5">
+        <button
+          onClick={onAddTask}
+          className="text-lg text-white bg-orange-300 hover:bg-orange-500 rounded-md p-2.5"
+        >
           追加
         </button>
       </section>
@@ -27,64 +36,91 @@ const InputTask = () => {
   );
 };
 
-const TaskListRow = () => {
+type Task = {
+  id: number;
+  title: string;
+};
+
+type TaskListRowProps = {
+  tasks: Task[];
+  onDeleteTask: (taskId: number) => void;
+};
+
+const TaskListRow: FC<TaskListRowProps> = ({ tasks, onDeleteTask }) => {
   return (
-      <ul className="w-4/5 md:w-5/6 lg:w-full mx-auto space-y-4">
-        <li className="flex items-center justify-around p-2 md:p-3 rounded-md bg-orange-50">
+    <ul className="w-4/5 md:w-5/6 lg:w-full mx-auto space-y-4">
+      {tasks.map((task) => (
+        <li
+          key={task.id}
+          className="flex items-center justify-around p-2 md:p-3 rounded-md bg-orange-50"
+        >
           <div className="flex items-center space-x-2">
-            <input type="checkbox" id="checkbox1" className="w-4 h-4" />
-            <label htmlFor="checkbox1" className="text-lg md:text-xl">
-              タスク1
+            <input type="checkbox" id={`${task.id}`} className="w-4 h-4" />
+            <label htmlFor={`${task.id}`} className="text-lg md:text-xl">
+              {task.title}
             </label>
           </div>
-          <button className="text-red-500 bg-red-100 p-2 rounded-md hover:bg-red-200 hover:shadow-md">
+          <button onClick={() => onDeleteTask(task.id)} className="text-red-500 bg-red-100 p-2 rounded-md hover:bg-red-200 hover:shadow-md">
             消す
           </button>
         </li>
-        <li className="flex items-center justify-around p-2 md:p-3 rounded-md bg-orange-50">
-          <div className="flex items-center space-x-2">
-            <input type="checkbox" id="checkbox2" className="w-4 h-4" />
-            <label htmlFor="checkbox2" className="text-lg md:text-xl">
-              タスク2
-            </label>
-          </div>
-          <button className="text-red-500 bg-red-100 p-2 rounded-md hover:bg-red-200 hover:shadow-md">
-            消す
-          </button>
-        </li>
-      </ul>
+      ))}
+    </ul>
   );
 };
 
-const TaskListView = () => {
+type TaskListViewProps = {
+  tasks: Task[];
+  onDeleteTask: (taskId: number) => void;
+};
+const TaskListView: FC<TaskListViewProps> = ({ tasks, onDeleteTask }) => {
   return (
     <>
       <section className="mb-10 max-w-xl w-full">
         <h2 className="text-xl md:text-2xl text-center font-semibold text-gray-700 mb-6">
           ・ 終わったらチェック !
         </h2>
-        <TaskListRow />
+        <TaskListRow tasks={tasks} onDeleteTask={onDeleteTask} />
       </section>
     </>
   );
 };
 
-const ScoreItem = () => {
+type ScoreItemProps = {
+  score: number;
+  label: string;
+  color: string;
+};
+const ScoreItem: FC<ScoreItemProps> = ({ score, label, color }) => {
   return (
     <>
-      <div className="bg-green-100 p-4 rounded-md text-center w-3/4 md:w-1/2 space-y-3 mx-auto mb-3 md:mb-0">
-        <span className="text-5xl md:text-6xl font-bold font-mono">1</span>
-        <p className="text-green-500 font-semibold">タスク進捗スコア</p>
-      </div>
-      <div className="bg-red-200 p-4 rounded-md text-center w-3/4 md:w-1/2 space-y-3 mx-auto">
-        <span className="text-5xl md:text-6xl font-bold font-mono">2</span>
-        <p className="text-red-500 font-semibold">全タスク-1のスコア</p>
+      <div
+        className={`${color} p-4 rounded-md text-center w-3/4 md:w-1/2 space-y-3 mx-auto mb-3 md:mb-0`}
+      >
+        <span className="text-5xl md:text-6xl font-bold font-mono">
+          {score}
+        </span>
+        <p
+          className={`font-semibold ${color === "bg-green-100" ? "text-green-500" : "text-red-500"}`}
+        >
+          {label}
+        </p>
       </div>
     </>
   );
 };
 
-const ScoreBoard = () => {
+type Score = {
+  score: number;
+  label: string;
+  color: string;
+};
+
+type ScoreBoardProps = {
+  scores: Score[];
+};
+
+const ScoreBoard: FC<ScoreBoardProps> = ({ scores }) => {
   return (
     <>
       <section className="mb-8 max-w-xl md:max-w-2xl w-full">
@@ -92,7 +128,9 @@ const ScoreBoard = () => {
           ・ 現在のスコア
         </h2>
         <div className="flex flex-col md:flex-row md:space-x-6">
-          <ScoreItem />
+          {scores.map((scoreItem, index) => (
+            <ScoreItem key={index} score={scoreItem.score} label={scoreItem.label} color={scoreItem.color} />
+          ))}
         </div>
       </section>
     </>
@@ -100,13 +138,29 @@ const ScoreBoard = () => {
 };
 
 export const TaskBattle = () => {
+  const tasks: Task[] = [
+    { id: 1, title: "タスク1" },
+    { id: 2, title: "タスク2" },
+  ];
+  const scores: Score[] = [
+    { score: 1, label: "タスク進捗スコア", color: "bg-green-100" },
+    { score: 2, label: "全タスク-1のスコア", color: "bg-red-200" },
+  ];
+  const handleAddTask = () => {
+    console.log("タスクを追加");
+  };
+
+  const handleDeleteTask = (taskId: number) => {
+    console.log("タスクを削除", taskId);
+  };
+
   return (
     <>
       <main className="flex flex-col items-center p-8 bg-blue-50 min-h-screen ">
         <Title />
-        <InputTask />
-        <TaskListView />
-        <ScoreBoard />
+        <InputTask onAddTask={handleAddTask} />
+        <TaskListView tasks={tasks} onDeleteTask={handleDeleteTask}  />
+        <ScoreBoard scores={scores} />
       </main>
     </>
   );
