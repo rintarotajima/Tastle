322bdadca503baf80c2432466afd1158059b8d54
rintarotajima <rintarotajima@gmail.com>
2024-11-06 21:02:04 +0900

    [ add ] Separate components in components folder

---
+++ src/components/DisplayTaskSection/DeleteTaskButton.tsx
@@ -0,0 +1,9 @@
+export const DeleteTaskButton = () => {
+  return (
+    <>
+      <button className="text-red-500 bg-red-100 p-2 rounded-md hover:bg-red-200 hover:shadow-md">
+        消す
+      </button>
+    </>
+  );
+};
