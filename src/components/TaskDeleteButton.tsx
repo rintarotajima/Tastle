3e84890553e4db762702e23a2bc682e2a9b14083
rintarotajima <rintarotajima@gmail.com>
2024-11-12 23:38:47 +0900

    [ add] 消すボタンコンポーネント作成

---
+++ src/components/TaskDeleteButton.tsx
@@ -0,0 +1,7 @@
+export const TaskDeleteButton = () => {
+  return (
+    <button className="text-red-500 bg-red-100 p-2 rounded-md hover:bg-red-200 hover:shadow-md">
+      消す
+    </button>
+  );
+};
