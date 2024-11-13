7f9cede7006052927225e96b4de1bbeb9e5dfb4c
rintarotajima <rintarotajima@gmail.com>
2024-11-13 17:44:19 +0900

    [ add ] タスク削除機能の実装

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -4,17 +4,25 @@ import { TaskDeleteButton } from "./TaskDeleteButton";
 
 type Props = {
   task: Task;
+  onDelete: (taskId: number) => void;
 };
 
 // 各タスクを表示するコンポーネント
-export const TaskItem: FC<Props> = ({ task }) => {
+export const TaskItem: FC<Props> = ({ task, onDelete }) => {
   return (
     <li className="flex items-center justify-around p-2 md:p-3 rounded-md bg-orange-50">
       <div className="flex items-center space-x-2">
-        <input type="checkbox" name={`${task.id}`} id={`${task.id}`} className="w-4 h-4" />
-        <label htmlFor={`${task.id}`} className="text-lg md:text-xl">{task.title}</label>
+        <input
+          type="checkbox"
+          name={`${task.id}`}
+          id={`${task.id}`}
+          className="w-4 h-4"
+        />
+        <label htmlFor={`${task.id}`} className="text-lg md:text-xl">
+          {task.title}
+        </label>
       </div>
-      <TaskDeleteButton />
+      <TaskDeleteButton taskId={task.id} onDelete={onDelete} />
     </li>
   );
 };
