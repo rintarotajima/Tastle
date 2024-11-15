ed386e950a5ee162e00394a450699e9a730a0116
rintarotajima <rintarotajima@gmail.com>
2024-11-15 23:13:04 +0900

    [ add ] スコア管理コンポーネントの作成

--- src/components/Tastle.tsx
+++ src/components/Tastle.tsx
@@ -1,19 +1,14 @@
 import { Title } from "./Title";
-import { Score } from "../types";
-import { DisplayScoreBoard } from "./DisplayScoreBoard";
 import { TaskManager } from "./TaskManager";
+import { ScoreManager } from "./ScoreManager";
 
 export const Tastle = () => {
-  const score: Score[] = [
-    { score: 1, label: "タスク進捗スコア", color: "bg-green-100" },
-    { score: 2, label: "全タスク数-1スコア", color: "bg-red-200" },
-  ];
 
   return (
     <main className="flex flex-col items-center p-8 bg-blue-50 min-h-screen">
       <Title />
       <TaskManager />
-      <DisplayScoreBoard scores={score} />
+      <ScoreManager />
     </main>
   );
 };
